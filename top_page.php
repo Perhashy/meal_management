eb1f6a90e8e39558af42072418a229c9c9ef5ceb
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-12 23:43:03 +0900

    トップページhtml

--- top_page.php
+++ top_page.php
@@ -3,13 +3,27 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>食事管理アプリ（仮</title>
     <link rel="stylesheet" href="styles/application.css">
   </head>
 
   <body>
-    <p>トップページです</p>
-    <a href="users/login.php">ログイン</a>
-    <a href="users/sign_up.php">新規登録ページ</a>
+    <div class="wrapper">
+      <div class="header">
+        <h1>ロゴ画像</h1>
+      </div>
+    </div>
+    <div class="container">
+      <div class="introduction">
+        <p>このサイトは食事管理アプリ（仮）です。</p>
+        <p>
+          簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。簡単な説明。
+        </p>
+      </div>
+      <div class="link">
+        <a href="users/login.php">ログイン</a>
+        <a href="users/sign_up.php">新規登録ページ</a>
+      </div>
+    </div>
   </body>
 </html>
