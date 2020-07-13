58a3f3a51c3a9b39982df070eceee5e282e4261b
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-13 19:09:46 +0900

    一覧表

--- index.php
+++ index.php
@@ -21,10 +21,39 @@
 
         <div class="contents-list">
           <h2 class="title">食べたものリスト</h2>
-          
           <a href="contents/new.php">新規投稿ページ</a>
-          <a href="contents/edit.php">編集ページ</a>
-          <a href="contents/delete.php">削除</a>
+          <table border="1">
+            <tr>
+              <th>名前</th>
+              <th>エネルギー(kcal)</th>
+              <th>たんぱく質(g)</th>
+              <th>脂質(g)</th>
+              <th>炭水化物(g)</th>
+              <th>塩分相当量(g)</th>
+              <th></th>
+              <th></th>
+            </tr>
+            <tr>
+              <td>アーモンド 100g</td>
+              <td>660</td>
+              <td>18.9</td>
+              <td>56.4</td>
+              <td>19.1</td>
+              <td>0.0</td>
+              <td><a href="contents/edit.php">編集ページ</a></td>
+              <td><a href="contents/delete.php">削除</a></td>
+            </tr>
+            <tr>
+              <td>アーモンド 100g</td>
+              <td>660</td>
+              <td>18.9</td>
+              <td>56.4</td>
+              <td>19.1</td>
+              <td>0.0</td>
+              <td><a href="contents/edit.php">編集ページ</a></td>
+              <td><a href="contents/delete.php">削除</a></td>
+            </tr>
+          </table>
         </div>
 
         <div class="contents-each">
