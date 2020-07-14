aebe949dcc699fde213c57ad86ceac86b382ca35
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-15 03:09:23 +0900

    投稿ページcss

--- contents/new.php
+++ contents/new.php
@@ -12,41 +12,44 @@
       <div class="header">
         <h1>食事管理アプリ（仮</h1>
       </div>
-    </div>
-    <div class="form">
-      <h1>食べたものを追加</h1>
-      <div class="form-title">
-        <h2>名前</h2>
-        <input type="text">
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">エネルギー</h2>
-        <input type="text">
-        <p>kcal</p>
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">たんぱく質</h2>
-        <input type="text">
-        <p>g</p>
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">脂質</h2>
-        <input type="text">
-        <p>g</p>
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">炭水化物</h2>
-        <input type="text">
-        <p>g</p>
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">塩分相当量</h2>
-        <input type="text">
-        <p>g</p>
-      </div>
-      <div class="form-box">
-        <h2 class="form-box-label">日付</h2>
-        <input type="date">
+      <div class="form">
+        <h1>食べたものを追加</h1>
+        <div class="form-title">
+          <h2>名前</h2>
+          <input type="text" class="form-title-input">
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">エネルギー：</h2>
+          <input type="text" class="form-box-input">
+          <p class="unit">kcal</p>
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">たんぱく質：</h2>
+          <input type="text" class="form-box-input">
+          <p class="unit">g</p>
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">脂質：　　　</h2>
+          <input type="text" class="form-box-input">
+          <p class="unit">g</p>
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">炭水化物：　</h2>
+          <input type="text" class="form-box-input">
+          <p class="unit">g</p>
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">塩分相当量：</h2>
+          <input type="text" class="form-box-input">
+          <p class="unit">g</p>
+        </div>
+        <div class="form-box">
+          <h2 class="form-box-label">日付：　　　</h2>
+          <input type="date" class="form-box-input" >
+        </div>
+        <div class="submit">
+          <input type="submit" class="submit-btn" value="追加する">
+        </div>
       </div>
     </div>
   </body>
