3502978d2353a16cfcb4e6e38f25c546e9b1d52b
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-16 20:52:48 +0900

    必須項目のエラー表示

--- users/sign_up.php
+++ users/sign_up.php
@@ -1,3 +1,17 @@
+<?php
+require_once('../config/config.php');
+
+if ($_POST['name'] === '') {
+  $error['name'] = 'blank';
+}
+if ($_POST['email'] === '') {
+  $error['email'] = 'blank';
+}
+if ($_POST['password'] === '') {
+  $error['password'] = 'blank';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -19,24 +33,30 @@
             <p class="title">・ニックネーム</p>
             <p class="must">必須</p>
             <div class="input">
-              <input type="text" name="name">
-              <p class="error">※ニックネームを入力してください</p>
+              <input type="text" name="name" value="<?= h($_POST['name']);?>">
+              <?php if ($error['name'] === 'blank'): ?>
+                <p class="error">※ニックネームを入力してください</p>
+              <?php endif; ?>
             </div>
           </div>
           <div class="form-box">
             <p class="title">・メールアドレス</p>
             <p class="must">必須</p>
             <div class="input">
-              <input type="text" name="email">
-              <p class="error">※メールアドレスを入力してください</p>
+              <input type="text" name="email" value="<?= h($_POST['email']);?>">
+              <?php if ($error['email'] === 'blank'): ?>
+                <p class="error">※メールアドレスを入力してください</p>
+              <?php endif; ?>
             </div>
           </div>
           <div class="form-box">
             <p class="title">・パスワード</p>
             <p class="must">必須</p>
             <div class="input">
-              <input type="text" name="password">
-              <p class="error">※パスワードを入力してください</p>
+              <input type="password" name="password" value="<?= h($_POST['password']);?>">
+              <?php if ($error['password'] === 'blank'): ?>
+                <p class="error">※パスワードを入力してください</p>
+              <?php endif; ?>
             </div>
           </div>
           <div class="submit">
