37c08ceefe7ddd8c6945aa46ed35837abb0339b0
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-16 22:03:51 +0900

    DBに接続し、保存

--- users/check.php
+++ users/check.php
@@ -1,8 +1,22 @@
 <?php
 require_once('../config/config.php');
+require_once('../config/dbconnect.php');
 
 if (!isset($_SESSION['user'])) {
   header('Location: sign_up.php');
+  exit();
+}
+
+if (!empty($_POST)) {
+  $statement = $db->prepare('INSERT INTO users SET name=?, email=?, password=?, created=NOW()');
+  $statement->execute(array(
+    $_SESSION['user']['name'],
+    $_SESSION['user']['email'],
+    sha1($_SESSION['user']['password'])
+  ));
+  unset($_SESSION['user']);
+
+  header('Location: thanks.php');
   exit();
 }
 ?>
@@ -22,24 +36,27 @@ if (!isset($_SESSION['user'])) {
         <h1>ユーザー登録</h1>
       </div>
       <div class="form">
-        <p class="form-message">記入した内容を確認して、「登録する」をクリックしてください</p>
-        <div class="form-box">
-          <p class="title">・ニックネーム</p>
-          <p class="content"><?= h($_SESSION['user']['name']);?></p>
-        </div>
-        <div class="form-box">
-          <p class="title">・メールアドレス</p>
-          <p class="content"><?= h($_SESSION['user']['email']);?></p>
-        </div>
-        <div class="form-box">
-          <p class="title">・パスワード</p>
-          <p class="content">【表示されません】</p>
-        </div>
-        <div class="submit">
-          <a href="sign_up.php?action=rewrite" class="submit-btn reedit">« 書き直す</a>
-          <input type="submit" class="submit-btn" value="登録する">
-        </div>
-        <a href="thanks.php">完了</a>
+        <form action="" method="post">
+          <input type="hidden" name="action" value="submit">
+          <p class="form-message">記入した内容を確認して、「登録する」をクリックしてください</p>
+          <div class="form-box">
+            <p class="title">・ニックネーム</p>
+            <p class="content"><?= h($_SESSION['user']['name']);?></p>
+          </div>
+          <div class="form-box">
+            <p class="title">・メールアドレス</p>
+            <p class="content"><?= h($_SESSION['user']['email']);?></p>
+          </div>
+          <div class="form-box">
+            <p class="title">・パスワード</p>
+            <p class="content">【表示されません】</p>
+          </div>
+          <div class="submit">
+            <a href="sign_up.php?action=rewrite" class="submit-btn reedit">« 書き直す</a>
+            <input type="submit" class="submit-btn" value="登録する">
+          </div>
+          <a href="thanks.php">完了</a>
+        </form>
       </div>
     </div>
   </body>
