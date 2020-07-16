f3091e37297a603bff50fe1ffc7d272d558b463e
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-16 20:23:16 +0900

    formタグ、name属性追加

--- users/sign_up.php
+++ users/sign_up.php
@@ -13,34 +13,36 @@
         <h1>ユーザー登録</h1>
       </div>
       <div class="form">
-        <p class="form-message">次のフォームに必要事項をご記入ください</p>
-        <div class="form-box">
-          <p class="title">・ニックネーム</p>
-          <p class="must">必須</p>
-          <div class="input">
-            <input type="text">
-            <p class="error">※ニックネームを入力してください</p>
+        <form action="" method="post">
+          <p class="form-message">次のフォームに必要事項をご記入ください</p>
+          <div class="form-box">
+            <p class="title">・ニックネーム</p>
+            <p class="must">必須</p>
+            <div class="input">
+              <input type="text" name="name">
+              <p class="error">※ニックネームを入力してください</p>
+            </div>
           </div>
-        </div>
-        <div class="form-box">
-          <p class="title">・メールアドレス</p>
-          <p class="must">必須</p>
-          <div class="input">
-            <input type="text">
-            <p class="error">※メールアドレスを入力してください</p>
+          <div class="form-box">
+            <p class="title">・メールアドレス</p>
+            <p class="must">必須</p>
+            <div class="input">
+              <input type="text" name="email">
+              <p class="error">※メールアドレスを入力してください</p>
+            </div>
           </div>
-        </div>
-        <div class="form-box">
-          <p class="title">・パスワード</p>
-          <p class="must">必須</p>
-          <div class="input">
-            <input type="text">
-            <p class="error">※パスワードを入力してください</p>
+          <div class="form-box">
+            <p class="title">・パスワード</p>
+            <p class="must">必須</p>
+            <div class="input">
+              <input type="text" name="password">
+              <p class="error">※パスワードを入力してください</p>
+            </div>
           </div>
-        </div>
-        <div class="submit">
-          <input type="submit" class="submit-btn" value="入力内容を確認">
-        </div>
+          <div class="submit">
+            <input type="submit" class="submit-btn" value="入力内容を確認">
+          </div>
+        </form>
         <a href="check.php">確認ページ</a>
       </div>
     </div>
