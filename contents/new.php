560a698026713e2980ad87e31a8994cfb27b1c2d
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-19 22:54:23 +0900

    formタグ作成

--- contents/new.php
+++ contents/new.php
@@ -14,42 +14,44 @@
       </div>
       <div class="form">
         <h1>食べたものを追加</h1>
-        <div class="form-title">
-          <h2>名前</h2>
-          <input type="text" class="form-title-input">
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">エネルギー：</h2>
-          <input type="text" class="form-box-input">
-          <p class="unit">kcal</p>
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">たんぱく質：</h2>
-          <input type="text" class="form-box-input">
-          <p class="unit">g</p>
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">脂質：　　　</h2>
-          <input type="text" class="form-box-input">
-          <p class="unit">g</p>
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">炭水化物：　</h2>
-          <input type="text" class="form-box-input">
-          <p class="unit">g</p>
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">塩分相当量：</h2>
-          <input type="text" class="form-box-input">
-          <p class="unit">g</p>
-        </div>
-        <div class="form-box">
-          <h2 class="form-box-label">日付：　　　</h2>
-          <input type="date" class="form-box-input" >
-        </div>
-        <div class="submit">
-          <input type="submit" class="submit-btn" value="追加する">
-        </div>
+        <form action="" method="post">
+          <div class="form-title">
+            <h2>名前</h2>
+            <input type="text" class="form-title-input" name="name">
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">エネルギー：</h2>
+            <input type="text" class="form-box-input" name="calorie">
+            <p class="unit">kcal</p>
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">たんぱく質：</h2>
+            <input type="text" class="form-box-input" name="protein">
+            <p class="unit">g</p>
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">脂質：　　　</h2>
+            <input type="text" class="form-box-input" name="lipid">
+            <p class="unit">g</p>
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">炭水化物：　</h2>
+            <input type="text" class="form-box-input" name="carbohydrate">
+            <p class="unit">g</p>
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">塩分相当量：</h2>
+            <input type="text" class="form-box-input" name="salt">
+            <p class="unit">g</p>
+          </div>
+          <div class="form-box">
+            <h2 class="form-box-label">日付：　　　</h2>
+            <input type="date" class="form-box-input" name="ate_date">
+          </div>
+          <div class="submit">
+            <input type="submit" class="submit-btn" value="追加する">
+          </div>
+        </form>
       </div>
     </div>
   </body>
