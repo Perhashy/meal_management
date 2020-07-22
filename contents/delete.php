8adab646f1f8d79d5bdf3240c45b20a15fc5bbd5
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-22 23:31:56 +0900

    削除ページ作成

--- contents/delete.php
+++ contents/delete.php
@@ -3,11 +3,21 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    <link rel="stylesheet" href="styles/application.css">
+    <title>食事管理アプリ（仮</title>
+    <link rel="stylesheet" href="../styles/application.css">
   </head>
 
   <body>
-    <p>削除ページ</p>
+    <div class="wrapper">
+      <div class="header">
+        <h1>食事管理アプリ（仮</h1>
+      </div>
+      <div class="message">
+        <h1>投稿を削除しました</h1>
+        <div class="submit">
+          <a href="../index.php" class="submit-btn">ホームへ</a>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
