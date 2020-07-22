06f53c4575544f48136855153094ee4599dab311
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-22 22:45:45 +0900

    ログイン中のデータの時のみ表示

--- contents/edit.php
+++ contents/edit.php
@@ -8,8 +8,19 @@ if (isset($_SESSION['id'])) {
   $users->execute(array($_SESSION['id']));
   $user = $users->fetch();
 
-  if (isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])) {
+  if (isset($_REQUEST['id']) && is_numeric($_SESSION['id'])) {
     $id = $_REQUEST['id'];
+
+    $posts = $db->prepare('SELECT * FROM posts WHERE id=?');
+    $posts->execute(array($id));
+    $post = $posts->fetch();
+    if ($post['user_id'] !== $user['id']) {
+      header('Location: ../index.php');
+      exit();
+    }
+  } else {
+    header('Location: ../index.php');
+      exit();
   }
 } else {
   header('Location: ../top_page.php');
@@ -40,39 +51,39 @@ if (isset($_SESSION['id'])) {
           <div class="form-title">
             <h2>名前</h2>
             <p class="must">必須</p>
-            <input type="text" class="form-title-input" name="name">
+            <input type="text" class="form-title-input" name="name" value="<?= $post['name'];?>">
             <?php if ($error['name'] === 'blank'): ?>
               <p class="error">※名前を入力してください</p>
             <?php endif;?>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">エネルギー：</h2>
-            <input type="number" min="0" class="form-box-input" name="calorie" value="<?= $_POST['calorie']?>">
+            <input type="number" min="0" class="form-box-input" name="calorie" value="<?= $post['calorie'];?>">
             <p class="unit">kcal</p>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">たんぱく質：</h2>
-            <input type="number" min="0" step="0.1" class="form-box-input" name="protein" value="<?= $_POST['protein']?>">
+            <input type="number" min="0" step="0.1" class="form-box-input" name="protein" value="<?= $post['protein'];?>">
             <p class="unit">g</p>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">脂質：　　　</h2>
-            <input type="number" min="0" step="0.1" class="form-box-input" name="lipid" value="<?= $_POST['lipid']?>">
+            <input type="number" min="0" step="0.1" class="form-box-input" name="lipid" value="<?= $post['lipid'];?>">
             <p class="unit">g</p>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">炭水化物：　</h2>
-            <input type="number" min="0" step="0.1" class="form-box-input" name="carbohydrate" value="<?= $_POST['carbohydrate']?>">
+            <input type="number" min="0" step="0.1" class="form-box-input" name="carbohydrate" value="<?= $post['carbohydrate'];?>">
             <p class="unit">g</p>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">塩分相当量：</h2>
-            <input type="number" min="0" step="0.1" class="form-box-input" name="salt" value="<?= $_POST['salt']?>">
+            <input type="number" min="0" step="0.1" class="form-box-input" name="salt" value="<?= $post['salt'];?>">
             <p class="unit">g</p>
           </div>
           <div class="form-box">
             <h2 class="form-box-label">日付：　　　</h2>
-            <input type="date" class="form-box-input" name="ate_date" value="<?= $date;?>">
+            <input type="date" class="form-box-input" name="ate_date" value="<?= $post['ate_date'];?>">
           </div>
           <div class="submit">
             <input type="submit" class="submit-btn" value="変更する">
